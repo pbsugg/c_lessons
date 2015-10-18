d9f7c0f5b22ef4252b951f558e959cb35315e6b6
Philip Sugg <pbensugg@gmail.com>
2015-10-18 15:58:14 -0500

    add timeElapsed file

---
+++ 8_structures/time_elapsed.c
@@ -0,0 +1,27 @@
+// #3, take function time_elapsed
+// input: two time structures
+// output: time between them
+
+#include <stdio.h>
+
+struct time =
+{
+  int hours;
+  int minutes;
+  int seconds;
+}
+
+
+int main(void)
+{
+
+
+
+}
+
+
+struct time timeElapsed(struct time timeOne, struct time timeTwo)
+{
+
+  
+}
